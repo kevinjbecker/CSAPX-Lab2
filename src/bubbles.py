4448f715e62ac971169774422a1ee0899a567910
Kevin <kjb2503@rit.edu>
2017-09-11 00:42:40 -0400

    Added DocStrings to the program

--- src/bubbles.py
+++ src/bubbles.py
@@ -1,9 +1,27 @@
-import turtle
-from random import random
-from math import pi
+# -*- coding: utf-8 -*-
+"""
+=========================
+| CSAPX Lab 2: Bubbles  |
+=========================
+
+A program that creates procedural, recursive turtle drawings based on user arguments.
+
+date: 09/09/2017
+author: Kevin Becker
+"""
+
+
+import turtle              # all the turtle
+from random import random  # random
+from math import pi        # pi
 
 
 def main():
+    """
+    This function prompts the user to specify the arguments and then creates the drawing based on their input.
+
+    :return: None
+    """
     initial_bubble_radius = int(input("Desired initial bubble radius (100 or more is suggested): "))
     fanout = int(input("Desired fanout (1 or more; a higher number will take longer): "))
     shrinkage = float(input("Desired shrinkage (between 0 and 1, exclusive): "))
@@ -15,12 +33,23 @@ def main():
     turtle.exitonclick()
 
 
-def draw_bubbles(radius: float, fanout: int, shrinkage: float, child_probability: float, current_probability: float = 1.0, count: int = 0)->float:
+def draw_bubbles(radius: float, fanout: int, shrinkage: float, initial_probability: float, current_probability: float = 1.0, count: int = 0)->float:
+    """
+    This function recursively creates the bubble drawing.
+
+    :param radius: the radius of the initial bubbles being drawn by the function.
+    :param fanout: the number of next-level bubbles to potentially be drawn.
+    :param shrinkage: the percentage of the bubbles radius to remain in the next level (ex. 100 @ .6 -> 60).
+    :param initial_probability: the initial probability of a child bubble being drawn.
+    :param current_probability: the current probability of a child bubble being drawn (ex initial_probability ** count + 1).
+    :param count: the current level of bubble being drawn.
+
+    :return: float, the area of the bubbles drawn from the returning circle to the furthest down child.
+    """
     colors = 'red', 'dark orange', 'yellow', 'lime green', 'dark turquoise', 'purple', 'deep pink'
     full_circle_degrees = 360  # the total number of degrees in a circle is 360
     half_circle_degrees = 180  # the total number of degrees in a half circle is 180
-    random_num = random()
-    if int(radius) <= 0 or (random_num > current_probability):
+    if random() > current_probability:
         """ so this only checks current_probability (and not the 
         count as well for the first time because current_probability is default at 1.0 for the first run
         which random could never get to it """
@@ -38,7 +67,7 @@ def draw_bubbles(radius: float, fanout: int, shrinkage: float, child_probability
             turtle.circle(radius, full_circle_degrees/fanout)
             turtle.right(half_circle_degrees)  # we use half_circle_degrees so that the next coming circle actually is
             # ask if keep the 1-shrinkage, or just leave it as is
-            area += draw_bubbles((radius * shrinkage), fanout, shrinkage, child_probability, (child_probability ** (count+1)), count+1)
+            area += draw_bubbles((radius * shrinkage), fanout, shrinkage, initial_probability, (initial_probability ** (count+1)), count+1)
             turtle.right(half_circle_degrees)
         area += pi * (radius ** 2)
     return area
