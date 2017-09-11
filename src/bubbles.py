a4824d930b0ec2654866dfc027a00628f7588405
Kevin <kjb2503@rit.edu>
2017-09-10 22:37:27 -0400

    Added main function

--- src/bubbles.py
+++ src/bubbles.py
@@ -1,28 +1,17 @@
 import turtle
-import random
+from random import random
+from math import pi
 
 
 def main():
-    initial_bubble_radius = int(input("Desired initial bubble radius (100 or more is suggested): "))
-    fanout = int(input("Desired fanout (1 or more; a higher number will take longer): "))
-    shrinkage = float(input("Desired shrinkage rate (between 0 and 1, exclusive): "))
-    child_probability = float(input("Desired child probability (between 0 and 1, inclusive): "))
+    initial_bubble_radius = 100  # int(input("Desired initial bubble radius (100 or more is suggested): "))
+    fanout = 2  # int(input("Desired fanout (1 or more; a higher number will take longer): "))
+    shrinkage = .75  # float(input("Desired shrinkage rate (between 0 and 1, exclusive): "))
+    child_probability = .25  # float(input("Desired child probability (between 0 and 1, inclusive): "))
     area = draw_bubbles(initial_bubble_radius, fanout, shrinkage, child_probability)
-    print(area)
-
-
-def draw_bubbles(radius: int, fanout: int, shrinkage: float, child_probability: float, count: int = 0)->float:
-    colors = 'chartreuse', 'salmon', 'cyan', 'turquoise', 'lavender', 'tan'
-    full_circle_degrees = 360 # the total number of degrees in a circle is 360
-    if radius <= 0:
-        return 0.0
-    else:
-        area = 0
-        turtle.fillcolor(colors[count])
-        turtle.begin_fill()
-        turtle.circle(radius, full_circle_degrees + (full_circle_degrees / fanout))
-        turtle.end_fill()
-        area += draw_bubbles((radius * shrinkage), fanout, shrinkage, child_probability, count+1)
+    print('The area of your drawing is', area)
+    print('Click anywhere on the screen to exit')
+    turtle.exitonclick()
 
 
 if __name__ == '__main__':
