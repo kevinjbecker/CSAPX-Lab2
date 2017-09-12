257b70b84271a33e4658a53697e2a81ed4966233
Kevin <kjb2503@rit.edu>
2017-09-12 16:08:10 -0400

    Final commenting

--- src/bubbles.py
+++ src/bubbles.py
@@ -11,7 +11,7 @@ author: Kevin Becker
 """
 
 
-import turtle              # all the turtle
+import turtle              # imports all of the turtle
 from random import random  # random
 from math import pi        # pi
 
@@ -27,15 +27,15 @@ def main():
 
     :return: None
     """
-    initial_bubble_radius = int(input("Desired initial bubble radius (100 or more is suggested): "))
-    fanout = int(input("Desired fanout (1 or more; a higher number will take longer): "))
-    shrinkage = float(input("Desired shrinkage (between 0 and 1, exclusive): "))
-    child_probability = float(input("Desired child probability (between 0 and 1, inclusive): "))
-    turtle.speed(0)
-    area = draw_bubbles(initial_bubble_radius, fanout, shrinkage, child_probability)
-    print('The area of your drawing is', area)
-    print('Click anywhere on the screen to exit')
-    turtle.exitonclick()
+    initial_bubble_radius = int(input("Desired initial bubble radius (100 or more is suggested): "))  # gathers the initial bubble radius from user
+    fanout = int(input("Desired fanout (1 or more; a higher number will take longer): "))  # gathers the fanout from user
+    shrinkage = float(input("Desired shrinkage (between 0 and 1, exclusive): "))  # gathers the shrinkage from user
+    child_probability = float(input("Desired child probability (between 0 and 1, inclusive): "))  # gathers the child_probability from the user
+    turtle.speed(0)  # this sets the speed of the animation to 0 so it doesn't take eleven years to complete it
+    area = draw_bubbles(initial_bubble_radius, fanout, shrinkage, child_probability)  # begins the bubblage
+    print('The area of your drawing is', area)  # prints the area of the entire circle
+    print('Click anywhere on the screen to exit')  # Tells the user they must exit by clicking on the drawing
+    turtle.exitonclick()  # actually makes the previous print truthful
 
 
 def draw_bubbles(radius: float, fanout: int, shrinkage: float, initial_probability: float, current_probability: float = 1.0, count: int = 0)->float:
@@ -51,27 +51,26 @@ def draw_bubbles(radius: float, fanout: int, shrinkage: float, initial_probabili
 
     :return: float, the area of the bubbles drawn from the returning circle to the furthest down child.
     """
-    if random() > current_probability:
-        """ The only way to stop creating children is if the random is greater than the current_probability (it will never be true for 1 """
+    if random() > current_probability:  # base case ending a child bubble tree
         return 0.0
-    else:
-        area = 0
-        turtle.pencolor(COLORS[count % len(COLORS)])
-        turtle.fillcolor(COLORS[count % len(COLORS)])
-        turtle.down()
-        turtle.begin_fill()
-        turtle.circle(radius, FULL_CIRCLE_DEGREES)
-        turtle.up()
-        turtle.end_fill()
+    else:  # all of this is only run if the tree continues down
+        area = 0  # set the area to 0 (just so when area += (stuff) is run, python doesn't complain)
+        turtle.pencolor(COLORS[count % len(COLORS)])  # sets the pen color to the correct color based on the count
+        turtle.fillcolor(COLORS[count % len(COLORS)])  # sets the fill color to the same color
+        turtle.down()  # places the pen down (this happens so that the border of the bubbles don't look ugly since it goes around twice)
+        turtle.begin_fill()  # begins filling the polygons being created
+        turtle.circle(radius, FULL_CIRCLE_DEGREES)  # makes a circle with a radius of the radius parameter
+        turtle.up()  # picks the pen up (for the next part, it becomes more obvious why later on)
+        turtle.end_fill()  # stops filling
         for _ in range(0, fanout):
-            turtle.circle(radius, FULL_CIRCLE_DEGREES/fanout)
-            turtle.right(TURN_AROUND)  # we use TURN_AROUND so that the next coming circle actually is
-            # ask if keep the 1-shrinkage, or just leave it as is
-            area += draw_bubbles((radius * shrinkage), fanout, shrinkage, initial_probability, (initial_probability ** (count+1)), count+1)
-            turtle.right(TURN_AROUND)
-        area += pi * (radius ** 2)
-    return area
+            turtle.circle(radius, FULL_CIRCLE_DEGREES/fanout)  # goes around the circle 360/fanout degrees to begin the child stage
+            turtle.right(TURN_AROUND)  # we use TURN_AROUND so that the next coming circle actually is oriented correctly
+            area += draw_bubbles((radius * shrinkage), fanout, shrinkage, initial_probability, (initial_probability ** (count+1)), count+1)  # makes a child tree
+            turtle.right(TURN_AROUND)  # turns right again so it continues on the correct path after this child tree is complete
+        area += pi * (radius ** 2)  # adds to the area the area of the current circle
+    return area  # returns the area
 
 
+# does the thing that runs main if no arguments are given (should be run every time since I didn't implement a command line method)
 if __name__ == '__main__':
     main()
